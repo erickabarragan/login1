9839d3954652395a487fe6af55d30af34aa59e8e
erickabarragan <99425696+erickabarragan@users.noreply.github.com>
2023-07-13 14:01:21 -0500

    se quitaron algunas importaciones a UsuariosControlador.java

--- Login/Login/src/main/java/com/example/Login/controlador/UsuariosControlador.java
+++ Login/Login/src/main/java/com/example/Login/controlador/UsuariosControlador.java
@@ -11,17 +11,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.Login.modelo.Usuarios;
 import com.example.Login.repositorio.UsuariosRepositorio;
 import com.example.Login.servicio.UsuariosServicios;
 
-import lombok.Data;
+
 
 @RestController
 @CrossOrigin(origins = "*")
